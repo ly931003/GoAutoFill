3dd40a73aa926200379b349f3c92b67bcfb1c1bb
GitHub Action <action@github.com>
2025-01-16 02:18:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/ly931003/goautofill/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/ly931003/goautofill/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ly931003.goautofill.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
